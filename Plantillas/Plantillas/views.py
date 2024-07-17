adbc79b4ea86df832255175a1963d816372bf651
AlexiRFlores <albertoramirez503hombre@gmail.com>
2024-07-17 16:47:33 -0600

    actualizacion reciente

--- Plantillas/Plantillas/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-def simple(request):
-    return render(request, 'simple.html', {})
-
-def dinamico(request, name):
-    categories = ['code',  'design','marketing', 'Hola', 'Bien', 'Alexi']
-    contexto = {'name':name, 'categories':categories}
-    return render(request, 'dinamico.html', contexto)
-
